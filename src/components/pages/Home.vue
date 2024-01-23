4e574450bc6e5aa7e32d8b225a1a09e22556abed
NikBaranovich <archosgp1975@gmail.com>
2024-01-23 10:08:34 +0200

    Add home page

---
+++ src/components/pages/Home.vue
@@ -0,0 +1,29 @@
+<template lang="">
+<div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://image.tmdb.org/t/p/w500/rz8GGX5Id2hCW1KzAIY4xwbQw1w.jpg" class="d-block w-20" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="http://localhost:8080/storage/event_images/4d7rDbDSaPXnbwEa4N5jzdHoEy3VF05fW7UYhBFn.jpg" class="d-block w-120" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="http://localhost:8080/storage/event_images/4d7rDbDSaPXnbwEa4N5jzdHoEy3VF05fW7UYhBFn.jpg" class="d-block w-20" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</template>
+<script setup>
+
+</script>
+<style lang="">
+    
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import './assets/main.css'
+import router from "@/router/router.js";
+import 'bootstrap/dist/js/bootstrap.bundle.js';
 
+import {createPinia} from "pinia";
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const pinia = createPinia();
+
+createApp(App)
+        .use(router)
+        .use(pinia)
+        .mount('#app')
